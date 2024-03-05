211f3bbc01677d91ff19fb06206a27fe1341b37a
James Kebinger <james.kebinger@prefab.cloud>
2024-03-05 17:41:27 -0600

    introduce decryption and hashing support types

---
+++ hashing.go
@@ -0,0 +1,21 @@
+package prefab
+
+import (
+	"fmt"
+	"github.com/spaolacci/murmur3"
+	"math"
+)
+
+type Hashing struct {
+}
+
+// HashZeroToOne
+func (Hashing) HashZeroToOne(value string) (hashedValue float64, ok bool) {
+	h32 := murmur3.New32()
+	_, err := h32.Write([]byte(value))
+	if err != nil {
+		fmt.Println("Error hashing data:", err)
+		return 0, false
+	}
+	return float64(h32.Sum32()) / float64(math.MaxUint32), true
+}
