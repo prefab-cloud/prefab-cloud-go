bcc307fa37adaf604e30bd64df967763e8363e47
James Kebinger <james.kebinger@prefab.cloud>
2024-03-08 16:01:26 -0600

    Rough initial versions of client, httpclient, options and a version file

---
+++ client.go
@@ -0,0 +1,134 @@
+package prefab
+
+import (
+	"errors"
+	"fmt"
+	prefabProto "github.com/prefab-cloud/prefab-cloud-go/proto"
+	"sync"
+	"time"
+)
+
+type Client struct {
+	options                         *Options
+	httpClient                      *HttpClient
+	configStore                     *ApiConfigStore
+	configResolver                  *ConfigResolver
+	initializationComplete          chan struct{}
+	closeInitializationCompleteOnce sync.Once
+}
+
+func NewClient(options Options) (*Client, error) {
+	httpClient, err := BuildHttpClient(options)
+	if err != nil {
+		panic(err)
+	}
+
+	configStore := BuildApiConfigStore()
+	configResolver := NewConfigResolver(configStore)
+
+	client := Client{options: &options, httpClient: httpClient, configStore: configStore, configResolver: configResolver, initializationComplete: make(chan struct{})}
+	go client.fetchFromServer(0)
+	return &client, nil
+}
+
+func (c *Client) fetchFromServer(offset int32) {
+	configs, err := c.httpClient.Load(offset)
+	if err != nil {
+		panic(err)
+	}
+	c.configStore.SetFromConfigsProto(configs)
+
+	c.closeInitializationCompleteOnce.Do(func() {
+		close(c.initializationComplete)
+	})
+}
+
+func (c *Client) ManualLoad(configs *prefabProto.Configs) {
+	c.configStore.SetFromConfigsProto(configs)
+}
+
+func (c *Client) GetIntValueWithDefault(key string, contextSet ContextSet, defaultValue int64) (value int64, err error) {
+	value, err = c.internalGetIntValue(key, contextSet, &defaultValue)
+	return value, err
+}
+
+func (c *Client) GetIntValue(key string, contextSet ContextSet) (value int64, err error) {
+	value, err = c.internalGetIntValue(key, contextSet, nil)
+	return value, err
+}
+
+func (c *Client) internalGetIntValue(key string, contextSet ContextSet, defaultValue *int64) (value int64, err error) {
+	defaultValueAvailable := defaultValue != nil
+	if c.awaitInitialization() == TIMEOUT {
+		if defaultValueAvailable {
+			return *defaultValue, nil
+		}
+		if c.options.OnInitializationFailure == RAISE {
+			return 0, errors.New("initialization timeout and no default specified")
+		}
+	}
+	match, err := c.configResolver.ResolveValue(key, &contextSet)
+	if err != nil {
+		return 0, err
+	}
+	switch v := match.match.Type.(type) {
+	case *prefabProto.ConfigValue_Int:
+		return v.Int, nil
+	}
+	if defaultValueAvailable {
+		return *defaultValue, nil
+	}
+	return 0, errors.New("config did not produce an int and no default is specified")
+
+}
+
+func (c *Client) GetStringValueWithDefault(key string, contextSet ContextSet, defaultValue string) (value string, err error) {
+	value, err = c.internalGetStringValue(key, contextSet, &defaultValue)
+	return value, err
+}
+
+func (c *Client) GetStringValue(key string, contextSet ContextSet) (value string, err error) {
+	value, err = c.internalGetStringValue(key, contextSet, nil)
+	return value, err
+}
+
+func (c *Client) internalGetStringValue(key string, contextSet ContextSet, defaultValue *string) (value string, err error) {
+	defaultValueAvailable := defaultValue != nil
+	if c.awaitInitialization() == TIMEOUT {
+		if defaultValueAvailable {
+			return *defaultValue, nil
+		}
+		if c.options.OnInitializationFailure == RAISE {
+			return "", errors.New("initialization timeout and no default specified")
+		}
+	}
+	match, err := c.configResolver.ResolveValue(key, &contextSet)
+	if err != nil {
+		return "", err
+	}
+	switch v := match.match.Type.(type) {
+	case *prefabProto.ConfigValue_String_:
+		return v.String_, nil
+	}
+	if defaultValueAvailable {
+		return *defaultValue, nil
+	}
+	return "", errors.New("config did not produce an int and no default is specified")
+}
+
+func (c *Client) awaitInitialization() (result awaitInitializationResult) {
+	select {
+	case <-c.initializationComplete:
+		return SUCCESS
+	case <-time.After(time.Duration(c.options.InitializationTimeoutSeconds) * time.Second):
+		fmt.Println("Timeout expired, proceeding without waiting further")
+		return TIMEOUT
+	}
+}
+
+type awaitInitializationResult int
+
+const (
+	SUCCESS awaitInitializationResult = iota
+	TIMEOUT
+)
---
+++ options.go
@@ -0,0 +1,63 @@
+package prefab
+
+import (
+	"errors"
+	"fmt"
+	"os"
+)
+
+type Datasource int
+
+const ApiKeyEnvVar = "PREFAB_API_KEY"
+const DomainEnvVar = "PREFAB_DOMAIN"
+
+const (
+	ALL Datasource = iota
+	LOCAL_ONLY
+)
+
+type Options struct {
+	ApiKey                       string
+	Datasource                   Datasource
+	PrefabDomain                 string
+	InitializationTimeoutSeconds int
+	OnInitializationFailure      OnInitializationFailure
+}
+
+var DefaultOptions = Options{ApiKey: "", PrefabDomain: "prefab.cloud", Datasource: ALL, InitializationTimeoutSeconds: 10, OnInitializationFailure: RAISE}
+
+func NewOptions(modifyFn func(*Options)) Options {
+	opts := DefaultOptions
+	modifyFn(&opts)
+	return opts
+}
+
+func (o *Options) apiKeySettingOrEnvVar() (string, error) {
+	if o.ApiKey == "" {
+		// Attempt to read from an environment variable if APIKey is not directly set
+		envAPIKey := os.Getenv(ApiKeyEnvVar)
+		if envAPIKey == "" {
+			return "", errors.New(fmt.Sprintf("API key is not set and not found in environment variable %s", ApiKeyEnvVar))
+		}
+		o.ApiKey = envAPIKey
+	}
+	return o.ApiKey, nil
+}
+
+func (o *Options) PrefabDomainEnvVarOrSetting() (string, error) {
+	domainFromEnvVar := os.Getenv(DomainEnvVar)
+	if domainFromEnvVar != "" {
+		o.PrefabDomain = domainFromEnvVar
+	}
+	if o.PrefabDomain == "" {
+		return "", errors.New("no PrefabDomain set")
+	}
+	return o.PrefabDomain, nil
+}
+
+type OnInitializationFailure int
+
+const (
+	RAISE  OnInitializationFailure = iota // RAISE = 0
+	UNLOCK                                // UNLOCK = 1
+)
