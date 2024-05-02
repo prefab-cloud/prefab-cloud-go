5557cb27aa6b35010b94af40c60e9fe50514729a
James Kebinger <james.kebinger@prefab.cloud>
2024-05-02 13:24:53 -0500

    add local config store w/ tests adds env names to options with default load from env var

---
+++ local_config_store.go
@@ -0,0 +1,49 @@
+package prefab
+
+import (
+	"fmt"
+	prefabProto "github.com/prefab-cloud/prefab-cloud-go/proto"
+	"os"
+	"path/filepath"
+)
+
+type LocalConfigStore struct {
+	source_directory string
+	configMap        map[string]*prefabProto.Config
+	initialized      bool
+}
+
+func NewLocalConfigStore(source_directory string, options *Options) *LocalConfigStore {
+	configMap := make(map[string]*prefabProto.Config)
+	env_names := append([]string{"default"}, options.EnvironmentNames...)
+	for _, env_name := range env_names {
+		file := filepath.Join(source_directory, ".prefab."+env_name+".config.yaml")
+		loadFileIntoMap(file, &configMap)
+	}
+	return &LocalConfigStore{source_directory: source_directory, configMap: configMap, initialized: true}
+
+}
+
+func loadFileIntoMap(filePath string, configmap *map[string]*prefabProto.Config) {
+	parser := &LocalConfigYamlParser{}
+	data, err := os.ReadFile(filePath)
+	if err != nil {
+		if os.IsNotExist(err) {
+			fmt.Printf("File %s does not exist", filePath)
+		}
+		return
+	}
+	configs, err := parser.parse(data)
+	if err != nil {
+		return
+	}
+	for _, config := range configs {
+		(*configmap)[config.Key] = config
+	}
+
+}
+
+func (s *LocalConfigStore) GetConfig(key string) (config *prefabProto.Config, exists bool) {
+	config, exists = s.configMap[key]
+	return config, exists
+}
