c722806acf48212d67af32f26825b80ab7d61e78
Jeffrey Chupp <jeff@semanticart.com>
2024-05-07 16:02:29 -0400

    Add makefile and .golangci.yml

--- scripts/compile-protos.sh
+++ scripts/compile-protos.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-#run from the rood directory
+#run from the root directory
 
 protoc --proto_path=proto-source \
---go_out=proto --go_opt=paths=source_relative \
---go_opt=Mproto-source/prefab.proto=github.com/prefab-cloud/prefab-cloud-go/proto \
-proto-source/prefab.proto
+  --go_out=proto --go_opt=paths=source_relative \
+  --go_opt=Mproto-source/prefab.proto=github.com/prefab-cloud/prefab-cloud-go/proto \
+  proto-source/prefab.proto
