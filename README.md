d49ae9d03714b86fd2a50abb8ab883e9d0eeec5a
Jeffrey Chupp <jeff@semanticart.com>
2024-06-12 13:56:22 -0400

    Fix extracting duration

--- README.md
+++ README.md
@@ -1,10 +1,3 @@
 # prefab-cloud-go
+
 Go client for prefab
-
-
-
-
-## proto compiling
-
-update the git submodule in ./proto-source
-`protoc --go_opt=paths=source_relative --go_out=./proto -I proto-source/  prefab.proto`
---
+++ utils/config_value_utils_test.go
@@ -0,0 +1,36 @@
+package utils_test
+
+import (
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/prefab-cloud/prefab-cloud-go/utils"
+
+	prefabProto "github.com/prefab-cloud/prefab-cloud-go/proto"
+)
+
+func TestExtractDurationValue(t *testing.T) {
+	t.Run("parsing durations", func(t *testing.T) {
+		value := &prefabProto.ConfigValue_Duration{Duration: &prefabProto.IsoDuration{Definition: "PT1M"}}
+		configValue := &prefabProto.ConfigValue{Type: value}
+
+		duration, ok := utils.ExtractDurationValue(configValue)
+
+		assert.True(t, ok)
+		assert.Equal(t, time.Minute, duration)
+	})
+}
+
+func TestExtractValue(t *testing.T) {
+	t.Run("parsing durations", func(t *testing.T) {
+		value := &prefabProto.ConfigValue_Duration{Duration: &prefabProto.IsoDuration{Definition: "PT1M"}}
+		configValue := &prefabProto.ConfigValue{Type: value}
+
+		extracted, ok, err := utils.ExtractValue(configValue)
+		assert.NoError(t, err)
+		assert.True(t, ok)
+		assert.Equal(t, time.Minute, extracted)
+	})
+}
